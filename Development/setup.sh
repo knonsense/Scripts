fdb57090efb59798b82241b7a33f80e155d670d4
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-18 18:49:23 -0700

    adding in folders for more scripts for different systems

---
+++ Development/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script Name:                  GitHub
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/18/2023
+# Purpose:                      Script that runs scripts for basic MacOs System Set up with SSH, RDP, Virtualbox and preferences.
+
+# Main
+
+
+# End
---
+++ WindowsScripts/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script Name:                  GitHub
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/18/2023
+# Purpose:                      Script that runs scripts for basic Windows System Set up with SSH, RDP, Virtualbox and preferences.
+
+# Main
+
+
+# End
